d6a21d2c7219dd0138815956c9aefd2502c747b3
woocash2 <lukasgniecki@gmail.com>
2025-02-21 17:23:16 +0100

    Add Timelocks contract

---
+++ contracts/CommonTimelocks.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: UNKNOWN
+pragma solidity ^0.8.28;
+
+abstract contract CommonTimelocks {
+    error ActionAlreadyRegistered(bytes32 actionHash);
+    error ActionNotRegistered(bytes32 actionHash);
+    error ActionTimelocked(bytes32 actionHash, uint256 lockedUntil);
+    error ActionNotTimelocked(bytes32 actionHash, uint256 lockedUntil);
+
+    // A special value for the timelock, which denotes that there is no registered timelock for the given action.
+    uint256 private constant NOT_REGISTERED = 0;
+
+    /// @custom:storage-location erc7201:common.storage.timelocks
+    struct TimelocksStorage {
+        mapping(bytes32 actionHash => uint256 lockedUntil) registeredTimelocks;
+    }
+
+    // keccak256(abi.encode(uint256(keccak256("common.storage.timelocks")) - 1)) & ~bytes32(uint256(0xff));
+    bytes32 private constant TIMELOCKS_STORAGE_LOCATION = 0xb4b5f37798c0578cab1322f006334977322f38ac9cf72880f6abeef244238800;
+    function _getTimelocksStorage() private pure returns (TimelocksStorage storage $) {
+        assembly {
+            $.slot := TIMELOCKS_STORAGE_LOCATION
+        }
+    }
+
+    // Adds a timelock entry for the given action if it doesn't exist yet.
+    function _register(bytes32 actionHash, uint256 delay) internal {
+        TimelocksStorage storage $ = _getTimelocksStorage();
+        if ($.registeredTimelocks[actionHash] != NOT_REGISTERED) {
+            revert ActionAlreadyRegistered(actionHash);
+        }
+        $.registeredTimelocks[actionHash] = block.timestamp + delay;
+    }
+
+    // Removes a timelock entry for the given action, if it exists, and the timelock has passed.
+    function _execute(bytes32 actionHash) internal {
+        TimelocksStorage storage $ = _getTimelocksStorage();
+        uint256 lockedUntil = $.registeredTimelocks[actionHash];
+        if (lockedUntil == NOT_REGISTERED) {
+            revert ActionNotRegistered(actionHash);
+        }
+        if (lockedUntil >= block.timestamp) {
+            revert ActionTimelocked(actionHash, lockedUntil);
+        }
+        delete $.registeredTimelocks[actionHash];
+    }
+
+    // Removes a timelock entry for the given action, if it exists, and the timelock has not passed yet.
+    function _cancel(bytes32 actionHash) internal {
+        TimelocksStorage storage $ = _getTimelocksStorage();
+        uint256 lockedUntil = $.registeredTimelocks[actionHash];
+        if (lockedUntil == NOT_REGISTERED) {
+            revert ActionNotRegistered(actionHash);
+        }
+        if (lockedUntil < block.timestamp) {
+            revert ActionNotTimelocked(actionHash, lockedUntil);
+        }
+        delete $.registeredTimelocks[actionHash];
+    }
+}
