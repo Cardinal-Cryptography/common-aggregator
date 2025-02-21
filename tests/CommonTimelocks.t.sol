8a6974e9bd551e4cba0430bd8d97d30a027b04bb
woocash2 <lukasgniecki@gmail.com>
2025-02-21 17:53:29 +0100

    Add initial tests for timelocks

---
+++ tests/CommonTimelocks.t.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: UNKNOWN
+pragma solidity ^0.8.28;
+
+import {Test} from "forge-std/Test.sol";
+import {CommonTimelocks} from "contracts/CommonTimelocks.sol";
+
+contract CommonTimelocksImpl is CommonTimelocks {}
+
+contract CommonTimelocksTest is Test {
+    CommonTimelocks timelocks;
+    
+    function setUp() public {
+        timelocks = new CommonTimelocksImpl();
+    }
+
+    function testSingleActionRegisterExecute() public {
+        vm.roll(1);
+        vm.warp(1000);
+
+        bytes32 actionHash = bytes32(0);
+        timelocks.register(actionHash, 100);
+
+        vm.roll(2);
+        vm.warp(1100);
+
+        vm.expectRevert(abi.encodeWithSelector(CommonTimelocks.ActionTimelocked.selector, actionHash, 1100));
+        timelocks.execute(actionHash);
+
+        vm.roll(3);
+        vm.warp(1101);
+        timelocks.execute(actionHash);
+    }
+
+    function testSingleActionRegisterCancel() public {
+        vm.roll(1);
+        vm.warp(1000);
+
+        bytes32 actionHash = bytes32(0);
+        timelocks.register(actionHash, 100);
+
+        vm.roll(2);
+        vm.warp(1101);
+
+        vm.expectRevert(abi.encodeWithSelector(CommonTimelocks.ActionNotTimelocked.selector, actionHash, 1100));
+        timelocks.cancel(actionHash);
+
+        vm.warp(1100); // earlier time, no roll
+        timelocks.cancel(actionHash);
+    }
+}
