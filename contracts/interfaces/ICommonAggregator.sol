4bc1ca4184cd4b2a396173aecef9d550b9677bf8
Grzegorz Gawryał <grzegorz.gawryal@cardinals.cc>
2025-02-21 16:37:01 +0100

    Derive ERC4626

--- contracts/interfaces/ICommonAggregator.sol
+++ contracts/interfaces/ICommonAggregator.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNKNOWN
 pragma solidity ^0.8.28;
 
-interface ICommonAggregator {
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface ICommonAggregator is IERC20 {
 
 }
--- tests/CommonAggregator.t.sol
+++ tests/CommonAggregator.t.sol
@@ -4,19 +4,25 @@ pragma solidity ^0.8.28;
 import {Test} from "forge-std/Test.sol";
 import {CommonAggregator} from "contracts/CommonAggregator.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {IERC4626} from "@openzeppelin/contracts/interfaces/IERC4626.sol";
+import {ERC4626Mock} from "tests/mock/ERC4626Mock.sol";
+import {ERC20Mock} from "tests/mock/ERC20Mock.sol";
+
 
 contract CommonAggregatorTest is Test {
     CommonAggregator commonAggregator;
     address owner = address(0x123);
 
     function setUp() public {
+        IERC20 asset = new ERC20Mock();
+
         CommonAggregator implementation = new CommonAggregator();
         IERC4626[] memory vaults = new IERC4626[](2);
-        vaults[0] = IERC4626(0x0000000000000000000000000000000000000001);
-        vaults[1] = IERC4626(0x0000000000000000000000000000000000000002);
+        vaults[0] = new ERC4626Mock(address(asset));
+        vaults[1] = new ERC4626Mock(address(asset));
 
-        bytes memory initializeData = abi.encodeWithSelector(CommonAggregator.initialize.selector, owner, vaults);
+        bytes memory initializeData = abi.encodeWithSelector(CommonAggregator.initialize.selector, owner, asset, vaults);
 
         ERC1967Proxy proxy = new ERC1967Proxy(address(implementation), initializeData);
         commonAggregator = CommonAggregator(address(proxy));
