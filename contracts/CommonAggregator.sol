a960a83bafae55607b3fada09f3ff8c031dd5933
Grzegorz Gawryał <grzegorz.gawryal@cardinals.cc>
2025-02-21 11:52:10 +0100

    Add basic namespace storage

--- contracts/CommonAggregator.sol
+++ contracts/CommonAggregator.sol
@@ -1,8 +1,9 @@
-// SPDX-License-Identifier: UNKNOWN
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
 import {ICommonAggregator} from "./interfaces/ICommonAggregator.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {IERC4626} from "@openzeppelin/contracts/interfaces/IERC4626.sol";
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import {AccessControlUpgradeable} from "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
@@ -13,16 +14,50 @@ contract CommonAggregator is ICommonAggregator, UUPSUpgradeable, AccessControlUp
     bytes32 public constant REBALANCER = keccak256("REBALANCER");
     bytes32 public constant GUARDIAN = keccak256("GUARDIAN");
 
-    constructor() {
-        _disableInitializers();
+    uint256 public constant MAX_VAULT_NUMBER = 5;
+    uint256 public constant MAX_BPS = 10000;
+
+    struct AggregatorStorage {
+        uint256 assetsCached;
+        IERC4626[] vaults; // Both for iterating and a fallback queue.
+        mapping(address vault => uint256 limit) allocationLimit; 
     }
 
-    function initialize(address admin) public initializer {
+    // keccak256(abi.encode(uint256(keccak256("commonAggregator.aggregator")) - 1)) & ~bytes32(uint256(0xff));
+    bytes32 private constant AGGREGATOR_STORAGE_LOCATION = 0xf8468fe7f3a9575c25bfcbae37ff341b77a73598911f3b2ece5f7b3088577e00;
+    function _getAggregatorStorage() private pure returns (AggregatorStorage storage $) {
+        assembly {
+            $.slot := AGGREGATOR_STORAGE_LOCATION
+        }
+    }
+
+    function initialize(address owner, IERC4626[] memory vaults) public initializer {
         __AccessControl_init();
         __UUPSUpgradeable_init();
 
-        _grantRole(DEFAULT_ADMIN_ROLE, admin);
-        _grantRole(OWNER, admin);
+        _grantRole(DEFAULT_ADMIN_ROLE, owner);
+        _grantRole(OWNER, owner);
+
+        AggregatorStorage storage $ = _getAggregatorStorage();
+        $.assetsCached = 0;
+
+        for (uint256 i = 0; i < vaults.length; i++) {
+            _ensureVaultCanBeAdded(address(vaults[i]));
+            $.vaults.push(vaults[i]);
+            $.allocationLimit[address(vaults[i])] = MAX_BPS;
+        }
+    }
+
+    function _ensureVaultCanBeAdded(address vault) private view {
+        AggregatorStorage storage $ = _getAggregatorStorage();
+        require($.vaults.length <  MAX_VAULT_NUMBER, "CommonAggregator: too many vaults");
+        for (uint256 i = 0; i < $.vaults.length; i++) {
+            require(address($.vaults[i]) != vault, "CommonAggregator: vault already exists");
+        }
+    }
+
+    constructor() {
+        _disableInitializers();
     }
 
     function _authorizeUpgrade(address newImplementation) internal override onlyRole(OWNER) {}
--- tests/CommonAggregator.t.sol
+++ tests/CommonAggregator.t.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.28;
 import {Test} from "forge-std/Test.sol";
 import {CommonAggregator} from "contracts/CommonAggregator.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {IERC4626} from "@openzeppelin/contracts/interfaces/IERC4626.sol";
 
 contract CommonAggregatorTest is Test {
     CommonAggregator commonAggregator;
@@ -11,7 +12,11 @@ contract CommonAggregatorTest is Test {
 
     function setUp() public {
         CommonAggregator implementation = new CommonAggregator();
-        bytes memory initializeData = abi.encodeWithSignature("initialize(address)", owner);
+        IERC4626[] memory vaults = new IERC4626[](2);
+        vaults[0] = IERC4626(0x0000000000000000000000000000000000000001);
+        vaults[1] = IERC4626(0x0000000000000000000000000000000000000002);
+
+        bytes memory initializeData = abi.encodeWithSelector(CommonAggregator.initialize.selector, owner, vaults);
 
         ERC1967Proxy proxy = new ERC1967Proxy(address(implementation), initializeData);
         commonAggregator = CommonAggregator(address(proxy));
