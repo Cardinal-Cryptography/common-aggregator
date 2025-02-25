e7e2aff755eb6a1cabe635d7b95d5597c0fdc374
Krzysztof Ziobro <86822080+krzysztofziobro@users.noreply.github.com>
2025-02-25 15:35:22 +0100

    [ENG-254] Reward buffer library (#3)
    * First attempt at lib for the reward buffer
    
    * Keep track of shares in the buffer + some tests (failing)
    
    * Tiny refactor
    
    * More tests + fix in buffered shares calculation
    
    * Add test workflow
    
    * Run test workflow on PRs
    
    * Setup pnpm in the workflow
    
    * Don't use nightly for workflow
    
    * Try alternative expectRevert config
    
    * Add test for larger values
    
    * Add new method for the buffer
    
    * fmt
    
    * Add simple fuzz test + checked arithmetic
    
    * Make _handle* view
    
    * Comment of PPS drop
    
    * Round up when burning shares to cover loss
    
    * Avg calculation in separate function
    
    * Custom error for zero cache
    
    * Remove underscores from local variables
    
    * rename
    
    * Add more fuzz
    
    * Add buffer update simulation
    
    * Test for all shares released
    
    * Release -> Burn rename
    
    * Spelling fix

---
+++ contracts/RewardBuffer.sol
@@ -0,0 +1,201 @@
+// SPDX-License-Identifier: UNKNOWN
+pragma solidity ^0.8.28;
+
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
+
+/// @title Buffer structure implementation for gradual reward release.
+/// Intended for usage within ERC-4626 vault implementations.
+library RewardBuffer {
+    using Math for uint256;
+
+    error AssetsCachedIsZero();
+
+    error AdditionOverflow(uint256 id);
+    error MultiplicationOverflow(uint256 id);
+    error DivisionByZero(uint256 id);
+    error SubtractionOverflow(uint256 id);
+
+    uint256 public constant DEFAULT_BUFFERING_DURATION = 20 days;
+
+    /// @dev MUST be initialized with non-zero value of `assetsCached` - vault should send some
+    /// assets (together with corresponding shares) to an unreachable address (in the constructor).
+    ///
+    /// Use `_newBuffer` in order to create new buffer instance.
+    struct Buffer {
+        uint256 assetsCached;
+        uint256 bufferedShares;
+        uint256 lastUpdate;
+        uint256 currentBufferEnd;
+    }
+
+    function _newBuffer(uint256 initialAssets) internal view returns (Buffer memory buffer) {
+        if (initialAssets == 0) revert AssetsCachedIsZero();
+        return Buffer(initialAssets, 0, block.timestamp, block.timestamp);
+    }
+
+    /// @dev Use this to implement `totalAssets()`.
+    function _getAssetsCached(Buffer storage buffer) internal view returns (uint256 assets) {
+        return buffer.assetsCached;
+    }
+
+    /// @dev Updates the buffer based on the current vault's state.
+    /// Should be called before any state mutating methods that depend on price-per-share.
+    ///
+    /// Alternatively (or additionally), it may be called by an off-chain component at times
+    /// when difference between `assetsCached` and `totalAssets()` becomes significant.
+    function _updateBuffer(Buffer storage buffer, uint256 totalAssets, uint256 totalShares)
+        internal
+        returns (uint256 sharesToMint, uint256 sharesToBurn)
+    {
+        Buffer memory memBuf = _toMemory(buffer);
+        (sharesToMint, sharesToBurn) = __updateBuffer(memBuf, totalAssets, totalShares);
+        _toStorage(memBuf, buffer);
+    }
+
+    /// @dev Simulates buffer update, returning the memory representation of an updated buffer.
+    function _simulateBufferUpdate(Buffer storage buffer, uint256 totalAssets, uint256 totalShares)
+        internal
+        view
+        returns (Buffer memory updatedBuffer, uint256 sharesToMint, uint256 sharesToBurn)
+    {
+        updatedBuffer = _toMemory(buffer);
+        (sharesToMint, sharesToBurn) = __updateBuffer(updatedBuffer, totalAssets, totalShares);
+    }
+
+    /// @dev Creates a `memory` copy of a buffer.
+    function _toMemory(Buffer storage buffer) internal view returns (Buffer memory memBuffer) {
+        memBuffer.assetsCached = buffer.assetsCached;
+        memBuffer.bufferedShares = buffer.bufferedShares;
+        memBuffer.lastUpdate = buffer.lastUpdate;
+        memBuffer.currentBufferEnd = buffer.currentBufferEnd;
+    }
+
+    /// @dev Copies `memory` buffer into storage.
+    function _toStorage(Buffer memory buffer, Buffer storage storageBuffer) internal {
+        storageBuffer.assetsCached = buffer.assetsCached;
+        storageBuffer.bufferedShares = buffer.bufferedShares;
+        storageBuffer.lastUpdate = buffer.lastUpdate;
+        storageBuffer.currentBufferEnd = buffer.currentBufferEnd;
+    }
+
+    function __updateBuffer(Buffer memory buffer, uint256 totalAssets, uint256 totalShares)
+        private
+        view
+        returns (uint256 sharesToMint, uint256 sharesToBurn)
+    {
+        if (buffer.assetsCached == 0) revert AssetsCachedIsZero();
+
+        // -- Rewards unlock --
+
+        sharesToBurn = _sharesToBurn(buffer);
+        buffer.bufferedShares = _checkedSub(buffer.bufferedShares, sharesToBurn, 1);
+        buffer.lastUpdate = block.timestamp;
+        buffer.currentBufferEnd = buffer.currentBufferEnd.max(block.timestamp);
+
+        // -- Buffer update (new rewards/loss) --
+
+        if (buffer.assetsCached <= totalAssets) {
+            (sharesToMint, buffer.currentBufferEnd) = _handleGain(buffer, totalShares, totalAssets);
+            buffer.bufferedShares = _checkedAdd(buffer.bufferedShares, sharesToMint, 2);
+        } else {
+            uint256 lossInShares = _handleLoss(buffer, totalShares, totalAssets);
+            sharesToBurn = _checkedAdd(sharesToBurn, lossInShares, 3);
+            buffer.bufferedShares = _checkedSub(buffer.bufferedShares, lossInShares, 4);
+        }
+
+        uint256 cancelledOut = sharesToBurn.min(sharesToMint);
+        sharesToBurn = _checkedSub(sharesToBurn, cancelledOut, 5);
+        sharesToMint = _checkedSub(sharesToMint, cancelledOut, 6);
+
+        buffer.assetsCached = totalAssets;
+    }
+
+    /// @dev Number of shares that should be burned to account for rewards to be released by the buffer.
+    /// Use it to implement `totalSupply()`.
+    function _sharesToBurn(Buffer memory buffer) internal view returns (uint256 sharesReleased) {
+        uint256 timestampNow = block.timestamp;
+        uint256 start = buffer.lastUpdate;
+        uint256 end = buffer.currentBufferEnd;
+        uint256 bufferedShares = buffer.bufferedShares;
+
+        if (end == start || timestampNow == start) {
+            return 0;
+        }
+
+        uint256 duration = _checkedSub(end, start, 7);
+        uint256 elapsed = _checkedSub(timestampNow, start, 8);
+
+        if (elapsed >= duration) {
+            sharesReleased = bufferedShares;
+        } else {
+            sharesReleased = bufferedShares.mulDiv(elapsed, duration);
+        }
+    }
+
+    function _handleGain(Buffer memory buffer, uint256 totalShares, uint256 totalAssets)
+        private
+        view
+        returns (uint256 sharesToMint, uint256 newBufferEnd)
+    {
+        uint256 gain = _checkedSub(totalAssets, buffer.assetsCached, 9);
+        sharesToMint = gain.mulDiv(totalShares, buffer.assetsCached);
+
+        if (sharesToMint == 0) {
+            return (0, buffer.currentBufferEnd);
+        }
+
+        uint256 newUnlockEnd = _checkedAdd(block.timestamp, DEFAULT_BUFFERING_DURATION, 10);
+        newBufferEnd = _weightedAvg(buffer.currentBufferEnd, buffer.bufferedShares, newUnlockEnd, sharesToMint);
+    }
+
+    function _handleLoss(Buffer memory buffer, uint256 totalShares, uint256 totalAssets)
+        private
+        pure
+        returns (uint256 sharesToBurn)
+    {
+        uint256 loss = _checkedSub(buffer.assetsCached, totalAssets, 11);
+        if (loss == 0) {
+            return 0;
+        }
+
+        uint256 lossInShares = loss.mulDiv(totalShares, buffer.assetsCached, Math.Rounding.Ceil);
+
+        // If we need to burn more than `buffer.bufferedShares` shares to retain price-per-share,
+        // then it's impossible to cover that from the buffer, and sharp PPS drop is to be expected.
+        sharesToBurn = lossInShares.min(buffer.bufferedShares);
+    }
+
+    function _weightedAvg(uint256 v1, uint256 w1, uint256 v2, uint256 w2) private pure returns (uint256 result) {
+        uint256 weightedVal1 = _checkedMul(w1, v1, 12);
+        uint256 weightedVal2 = _checkedMul(w2, v2, 13);
+
+        uint256 weightedSum = _checkedAdd(weightedVal1, weightedVal2, 14);
+        uint256 weightSum = _checkedAdd(w1, w2, 15);
+
+        result = _checkedDiv(weightedSum, weightSum, 16);
+    }
+
+    function _checkedAdd(uint256 a, uint256 b, uint256 id) private pure returns (uint256 result) {
+        bool success;
+        (success, result) = a.tryAdd(b);
+        if (!success) revert AdditionOverflow(id);
+    }
+
+    function _checkedMul(uint256 a, uint256 b, uint256 id) private pure returns (uint256 result) {
+        bool success;
+        (success, result) = a.tryMul(b);
+        if (!success) revert MultiplicationOverflow(id);
+    }
+
+    function _checkedDiv(uint256 a, uint256 b, uint256 id) private pure returns (uint256 result) {
+        bool success;
+        (success, result) = a.tryDiv(b);
+        if (!success) revert DivisionByZero(id);
+    }
+
+    function _checkedSub(uint256 a, uint256 b, uint256 id) private pure returns (uint256 result) {
+        bool success;
+        (success, result) = a.trySub(b);
+        if (!success) revert SubtractionOverflow(id);
+    }
+}
---
+++ tests/RewardBufferLib.t.sol
@@ -0,0 +1,234 @@
+// SPDX-License-Identifier: UNKNOWN
+pragma solidity ^0.8.28;
+
+import {Test} from "forge-std/Test.sol";
+import {RewardBuffer} from "../contracts/RewardBuffer.sol";
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
+import {console} from "forge-std/console.sol";
+
+contract RewardBufferTest is Test {
+    using RewardBuffer for RewardBuffer.Buffer;
+    using Math for uint256;
+
+    uint256 constant STARTING_TIMESTAMP = 100;
+    uint256 constant STARTING_BALANCE = 10;
+
+    RewardBuffer.Buffer buffer;
+
+    function setUp() public {
+        vm.warp(STARTING_TIMESTAMP);
+        buffer = RewardBuffer._newBuffer(STARTING_BALANCE);
+    }
+
+    function testCachedAssetsAfterInit() public view {
+        assertEq(buffer._getAssetsCached(), STARTING_BALANCE);
+    }
+
+    function testCachedAssetsAfterBufferUpdate() public {
+        buffer._updateBuffer(20, 200);
+        assertEq(buffer._getAssetsCached(), 20);
+    }
+
+    /// forge-config: default.allow_internal_expect_revert = true
+    function testUpdateRevertsWhenZeroStartingAssets() public {
+        buffer.assetsCached = 0;
+        vm.expectRevert(RewardBuffer.AssetsCachedIsZero.selector);
+        buffer._updateBuffer(20, 1000);
+    }
+
+    function testCachedAssetsAfterBufferUpdateAndTimeElapsed() public {
+        buffer._updateBuffer(20, 200);
+        vm.warp(STARTING_TIMESTAMP + 10 days);
+        assertEq(buffer._getAssetsCached(), 20);
+    }
+
+    function testSharesBurntAfterBufferUpdate() public {
+        buffer._updateBuffer(20, 100);
+        assertEq(buffer._sharesToBurn(), 0);
+    }
+
+    function testSharesBurntAfterBufferUpdateAndTimeElapsed() public {
+        buffer._updateBuffer(20, 100);
+        vm.warp(STARTING_TIMESTAMP + 2 days);
+        assertEq(buffer._sharesToBurn(), 10);
+    }
+
+    function testSharesBurntAfterBufferUpdateAndTimeElapsed2() public {
+        buffer._updateBuffer(17, 100);
+        vm.warp(STARTING_TIMESTAMP + 7 days);
+        assertEq(buffer._sharesToBurn(), 24);
+    }
+
+    function testSharesBurntAfterFullPeriodHasPassed() public {
+        buffer._updateBuffer(15, 100);
+        vm.warp(STARTING_TIMESTAMP + 20 days);
+        assertEq(buffer._sharesToBurn(), 50);
+    }
+
+    function testBufferUpdateResultOnGain() public {
+        (uint256 _toMint, uint256 _toBurn) = buffer._updateBuffer(12, 100);
+        assertEq(_toMint, 20);
+        assertEq(_toBurn, 0);
+    }
+
+    function testBufferUpdateResultOnLoss() public {
+        (uint256 _toMint, uint256 _toBurn) = buffer._updateBuffer(4, 100);
+        assertEq(_toMint, 0);
+        assertEq(_toBurn, 0);
+    }
+
+    function testBufferUpdateResultOnLoss2() public {
+        buffer._updateBuffer(100, 100);
+        (uint256 _toMint, uint256 _toBurn) = buffer._updateBuffer(70, 1000);
+        assertEq(_toMint, 0);
+        assertEq(_toBurn, 300);
+    }
+
+    function testBufferEndFirstUpdate() public {
+        buffer._updateBuffer(100, 100);
+        assertEq(buffer.currentBufferEnd, STARTING_TIMESTAMP + 20 days);
+    }
+
+    function testBufferEndSecondUpdateOldActive() public {
+        buffer._updateBuffer(20, 100);
+        vm.warp(STARTING_TIMESTAMP + 4 days);
+        buffer._updateBuffer(40, 200);
+
+        assertEq(buffer.currentBufferEnd, STARTING_TIMESTAMP + 4 days + uint256((16 days * 2 + 20 days * 5)) / 7);
+    }
+
+    function testBufferEndSecondUpdateElapsed() public {
+        buffer._updateBuffer(20, 100);
+        vm.warp(STARTING_TIMESTAMP + 40 days);
+        buffer._updateBuffer(40, 200);
+
+        assertEq(buffer.currentBufferEnd, STARTING_TIMESTAMP + 40 days + 20 days);
+    }
+
+    function testBigNumbers() public {
+        (uint256 _toMint, uint256 _toBurn) = buffer._updateBuffer(10 + (1 << 120), (1 << 5));
+        assertEq(_toMint, uint256(1 << 125) / 10);
+        assertEq(_toBurn, 0);
+    }
+
+    uint256 constant UPDATE_NUM = 10;
+
+    function testFuzz_BufferEndIsNeverLowerThanLastUpdate(
+        uint120[UPDATE_NUM] calldata _timeElapsed,
+        uint120[UPDATE_NUM] calldata _gain
+    ) public {
+        uint256 _currentTime = STARTING_TIMESTAMP;
+        uint256 _totalAssets = STARTING_BALANCE;
+        uint256 _totalShares = 100;
+
+        for (uint256 i = 0; i < UPDATE_NUM; ++i) {
+            _currentTime += _timeElapsed[i];
+            vm.warp(_currentTime);
+
+            _totalAssets += _gain[i];
+            (uint256 _toMint, uint256 _toBurn) = buffer._updateBuffer(_totalAssets, _totalShares);
+
+            assertLe(_toBurn, _totalShares);
+
+            _totalShares += _toMint;
+            _totalShares -= _toBurn;
+
+            assertLe(buffer.lastUpdate, buffer.currentBufferEnd);
+        }
+    }
+
+    function testFuzz_MonotonicPricePerShare(
+        uint120[UPDATE_NUM] calldata _timeElapsed,
+        uint120[UPDATE_NUM] calldata _gain
+    ) public {
+        uint256 _currentTime = STARTING_TIMESTAMP;
+        uint256 _totalAssets = STARTING_BALANCE;
+        uint256 _totalShares = 100;
+
+        uint256 _lastAssets = _totalAssets;
+        uint256 _lastShares = _totalShares;
+
+        for (uint256 i = 0; i < UPDATE_NUM; ++i) {
+            _currentTime += _timeElapsed[i];
+            vm.warp(_currentTime);
+
+            _totalAssets += _gain[i];
+            (uint256 _toMint, uint256 _toBurn) = buffer._updateBuffer(_totalAssets, _totalShares);
+
+            assertLe(_toBurn, _totalShares);
+
+            _totalShares += _toMint;
+            _totalShares -= _toBurn;
+
+            assertLe(_lastAssets * _totalShares, _totalAssets * _lastShares);
+
+            _lastAssets = _totalAssets;
+            _lastShares = _totalShares;
+        }
+    }
+
+    function testFuzz_BigValues(
+        uint120[UPDATE_NUM] calldata _timeElapsed,
+        uint128[UPDATE_NUM] calldata _gain,
+        uint120 _offset
+    ) public {
+        uint256 _totalGain = 0;
+        for (uint256 i = 0; i < UPDATE_NUM; ++i) {
+            _totalGain += _gain[i];
+        }
+
+        vm.assume(_totalGain * _offset < (1 << 128));
+
+        uint256 _currentTime = STARTING_TIMESTAMP;
+        uint256 _totalAssets = STARTING_BALANCE;
+        uint256 _totalShares = STARTING_BALANCE * _offset;
+
+        for (uint256 i = 0; i < UPDATE_NUM; ++i) {
+            _currentTime += _timeElapsed[i];
+            vm.warp(_currentTime);
+
+            _totalAssets += _gain[i];
+            (uint256 _toMint, uint256 _toBurn) = buffer._updateBuffer(_totalAssets, _totalShares);
+
+            assertLe(_toBurn, _totalShares);
+
+            _totalShares += _toMint;
+            _totalShares -= _toBurn;
+        }
+    }
+
+    function testFuzz_WithUserActions(
+        uint120[UPDATE_NUM] calldata _timeElapsed,
+        uint120[UPDATE_NUM] calldata _gain,
+        uint120[UPDATE_NUM] calldata _deposit,
+        uint120[UPDATE_NUM] calldata _withdraw
+    ) public {
+        uint256 _currentTime = STARTING_TIMESTAMP;
+        uint256 _totalAssets = STARTING_BALANCE;
+        uint256 _totalShares = 10000;
+
+        for (uint256 i = 0; i < UPDATE_NUM; ++i) {
+            _currentTime += _timeElapsed[i];
+            vm.warp(_currentTime);
+
+            _totalAssets += _gain[i];
+            (uint256 _toMint, uint256 _toBurn) = buffer._updateBuffer(_totalAssets, _totalShares);
+
+            assertLe(_toBurn, _totalShares);
+
+            _totalShares += _toMint;
+            _totalShares -= _toBurn;
+
+            uint256 _depositShares = uint256(_deposit[i]).mulDiv(_totalShares, _totalAssets);
+            _totalAssets += _deposit[i];
+            _totalShares += _depositShares;
+            buffer.assetsCached += _deposit[i];
+
+            uint256 _withdrawShares = uint256(_withdraw[i]).mulDiv(_totalShares, _totalAssets);
+            vm.assume(_withdrawShares < _totalShares - buffer.bufferedShares);
+            _totalAssets -= _withdraw[i];
+            _totalShares -= _withdrawShares;
+            buffer.assetsCached -= _withdraw[i];
+        }
+    }
+}
