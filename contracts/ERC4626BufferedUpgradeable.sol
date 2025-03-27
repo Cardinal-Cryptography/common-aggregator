242c45f4f07de6d175adcbad2d325d2c21ba2d31
Krzysztof Ziobro <jerzym27@protonmail.com>
2025-03-27 11:39:49 +0100

    Move common logic to _deposit and _withdraw

--- contracts/ERC4626BufferedUpgradeable.sol
+++ contracts/ERC4626BufferedUpgradeable.sol
@@ -407,8 +407,6 @@ contract ERC4626BufferedUpgradeable is Initializable, ERC20Upgradeable, IERC4626
 
         uint256 shares = previewDeposit(assets);
         _deposit(_msgSender(), receiver, assets, shares);
-        _postDeposit(assets);
-        _increaseAssets(assets);
 
         return shares;
     }
@@ -423,8 +421,6 @@ contract ERC4626BufferedUpgradeable is Initializable, ERC20Upgradeable, IERC4626
 
         uint256 assets = previewMint(shares);
         _deposit(_msgSender(), receiver, assets, shares);
-        _postDeposit(assets);
-        _increaseAssets(assets);
 
         return assets;
     }
@@ -437,7 +433,6 @@ contract ERC4626BufferedUpgradeable is Initializable, ERC20Upgradeable, IERC4626
         returns (uint256)
     {
         updateHoldingsState();
-        _preWithdrawal(assets);
         uint256 maxAssets = maxWithdraw(owner);
         if (assets > maxAssets) {
             revert ERC4626ExceededMaxWithdraw(owner, assets, maxAssets);
@@ -445,7 +440,6 @@ contract ERC4626BufferedUpgradeable is Initializable, ERC20Upgradeable, IERC4626
 
         uint256 shares = previewWithdraw(assets);
         _withdraw(_msgSender(), receiver, owner, assets, shares);
-        _decreaseAssets(assets);
 
         return shares;
     }
@@ -458,7 +452,6 @@ contract ERC4626BufferedUpgradeable is Initializable, ERC20Upgradeable, IERC4626
         returns (uint256)
     {
         updateHoldingsState();
-        _preWithdrawal(convertToAssets(shares));
         uint256 maxShares = maxRedeem(owner);
         if (shares > maxShares) {
             revert ERC4626ExceededMaxRedeem(owner, shares, maxShares);
@@ -466,7 +459,6 @@ contract ERC4626BufferedUpgradeable is Initializable, ERC20Upgradeable, IERC4626
 
         uint256 assets = previewRedeem(shares);
         _withdraw(_msgSender(), receiver, owner, assets, shares);
-        _decreaseAssets(assets);
 
         return assets;
     }
@@ -499,6 +491,8 @@ contract ERC4626BufferedUpgradeable is Initializable, ERC20Upgradeable, IERC4626
         // slither-disable-next-line reentrancy-no-eth
         SafeERC20.safeTransferFrom($._asset, caller, address(this), assets);
         _mint(receiver, shares);
+        _postDeposit(assets);
+        _increaseAssets(assets);
 
         emit Deposit(caller, receiver, assets, shares);
     }
@@ -521,8 +515,10 @@ contract ERC4626BufferedUpgradeable is Initializable, ERC20Upgradeable, IERC4626
         //
         // Conclusion: we need to do the transfer after the burn so that any reentrancy would happen after the
         // shares are burned and after the assets are transferred, which is a valid state.
+        _preWithdrawal(assets);
         _burn(owner, shares);
         SafeERC20.safeTransfer($._asset, receiver, assets);
+        _decreaseAssets(assets);
 
         emit Withdraw(caller, receiver, owner, assets, shares);
     }
